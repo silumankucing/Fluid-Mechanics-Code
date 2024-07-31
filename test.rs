d9c991e86c9a8137d5c8d6a667dcbc6e5a8b1cc3
angga saputra <anggalabs@gmail.com>
2024-07-31 17:07:00 +0700

    new

--- test.rs
+++ test.rs
@@ -1,14 +1,18 @@
 use std::io::{self, Write};
 
 fn main() {
-    let mut input = String::new();
+    let mut name = String::new();
+    let mut email = String::new();
 
-    print!("Please enter some text: ");
-    
+    print!("Enter Name: ");
+    print!("Enter Email: ");
+
     io::stdout().flush().expect("Failed to flush stdout");
     io::stdin().read_line(&mut input).expect("Failed to read line");
     
     let input = input.trim();
 
-    println!("You entered: {}", input);
+    println!("You entered: {}", name);
+    println!("You entered: {}", email);
+
 }
