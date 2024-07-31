177c23dfe01a013aa94a84d92e3fa75303924f7b
angga saputra <anggalabs@gmail.com>
2024-07-31 17:05:43 +0700

    nw

--- test.rs
+++ test.rs
@@ -1,5 +1,14 @@
-// code by quro
+use std::io::{self, Write};
 
 fn main() {
-    println!("Hello World!");
+    let mut input = String::new();
+
+    print!("Please enter some text: ");
+    
+    io::stdout().flush().expect("Failed to flush stdout");
+    io::stdin().read_line(&mut input).expect("Failed to read line");
+    
+    let input = input.trim();
+
+    println!("You entered: {}", input);
 }
